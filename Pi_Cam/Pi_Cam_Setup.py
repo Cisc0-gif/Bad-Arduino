6df1dcbda487b037ce6104a090c7343889f8d773
root <root@raspberrypi>
2020-01-30 13:22:18 -0800

    add HTTPAuth to Pi_cam snapshot server

--- Pi_Cam/Pi_Cam_Setup.py
+++ Pi_Cam/Pi_Cam_Setup.py
@@ -854,10 +854,62 @@ echo "[*]Starting motion daemon..."
 sudo service motion start
 echo "[*]Starting motion server..."
 sudo motion -c ~/.motion/motion.conf
+<<<<<<< HEAD
+echo "[*]Starting snapshot server in /var/lib/motion on port 9082"
+cd /var/lib/motion
+nohup python ~/.motion/SecureHTTPServer.py 9082 'pi:SuperHappyFace3' &
+=======
 echo "[*]Staring snapshot server in /var/lib/motion on port 9082"
 cd /var/lib/motion
 nohup sudo python -m SimpleHTTPServer 9082 &
+>>>>>>> 8333ff8cf88fe0354c43216267219108c9a899a5
 echo "[*]Armed!"
+"""
+
+#~/.motion/SecureHTTPServer.py
+SecureHTTPServer.py = """\
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import base64
+import BaseHTTPServer
+
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+
+class AuthenticationHandler(SimpleHTTPRequestHandler):
+    key = ''
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_AUTHHEAD(self):
+        self.send_response(401)
+        self.send_header('WWW-Authenticate', 'Basic realm=\"Secure HTTP Environment\"')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        if self.headers.getheader('Authorization') is None:
+            self.do_AUTHHEAD()
+            self.wfile.write('No auth received')
+        elif self.headers.getheader('Authorization') == 'Basic ' + self.key:
+            SimpleHTTPRequestHandler.do_GET(self)
+        else:
+            self.do_AUTHHEAD()
+            self.wfile.write(self.headers.getheader('Authorization'))
+            self.wfile.write('Not authenticated')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print "Usage: SecureHTTPServer.py [port] [username:password]"
+        sys.exit()
+    AuthenticationHandler.key = base64.b64encode(sys.argv[2])
+    BaseHTTPServer.test(AuthenticationHandler, BaseHTTPServer.HTTPServer)
 """
 
 with open('arm.sh', 'w+') as f: #write arm to ~/.motion/arm.sh
